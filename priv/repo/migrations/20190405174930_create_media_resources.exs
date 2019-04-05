8a3412cb3b9b4935f82c130a0f0eea27d1115850
umamaistempo <11341349+umamaistempo@users.noreply.github.com>
2019-04-05 20:13:39 +0200

    Configure the migration's schema for Media.Resource

--- priv/repo/migrations/20190405174930_create_media_resources.exs
+++ priv/repo/migrations/20190405174930_create_media_resources.exs
@@ -2,10 +2,15 @@ defmodule WebStream.Repo.Migrations.CreateMediaResources do
   use Ecto.Migration
 
   def change do
-    create table(:media_resources) do
+    execute("CREATE EXTENSION IF NOT EXISTS pgcrypto")
 
-      timestamps()
+    create table(:media_resources, primary_key: false) do
+      ## NOTE: Type is PostgreSQL's UUID4 because it optimizes the storage and indexation of it
+      ##    and by being an UUID4, other producers can generate their own UUID (or rely on the default generator)
+      add(:uuid, :uuid, primary_key: true, default: fragment("gen_random_uuid()"))
+      add(:fallback_title, :string, null: false, comment: "The title for the resource when a translation-specific is not provided")
+
+      timestamps(type: :utc_datetime)
     end
-
   end
 end
